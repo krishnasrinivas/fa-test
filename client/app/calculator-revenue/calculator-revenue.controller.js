1bac7bef009a3c91f4f72445a7cbd6d8e87264e9
Krishna Srinivas <krishna.srinivas@gmail.com>
2015-02-23 02:34:33 +0530

    Add repayment details with increments to /calculator-revenue route

--- client/app/calculator-revenue/calculator-revenue.controller.js
+++ client/app/calculator-revenue/calculator-revenue.controller.js
@@ -5,10 +5,41 @@ angular.module('faTestApp')
   	$scope.calc = {
   		termtype : "1"
   	}
+    $scope.rows = [];
     $scope.revenue = function() {
         return (Math.round($scope.calc.investmentAmount * Math.pow((1 + (($scope.calc.annualInterest*($scope.calc.termtype/12))/100)), $scope.calc.terms) * 100)/100);
     }
     $scope.profit = function() {
     	return (Math.round(($scope.revenue() - $scope.calc.investmentAmount)*100))/100;
     }
+    $scope.emi = function() {
+      var interestperterm = $scope.calc.annualInterest*($scope.calc.termtype/12)/100;
+      var x = Math.pow(1+interestperterm, $scope.calc.terms);
+      return Math.round($scope.calc.investmentAmount*interestperterm*x/(x-1)*100)/100;
+    }
+    $scope.totalInterest = function() {
+      return Math.round(($scope.emi()*$scope.calc.terms - $scope.calc.investmentAmount)*100)/100;
+    }
+
+    $scope.$watch($scope.emi,
+      function() {
+        $scope.rows = [];
+        var emi = $scope.emi();
+        var interestperterm = $scope.calc.annualInterest*($scope.calc.termtype/12)/100;
+        var balance = $scope.calc.investmentAmount;
+        for (var i = 0; i < $scope.calc.terms; i++) {
+          var obj = {
+            termno: i+1,
+            interest: Math.round(balance*interestperterm*100)/100,
+            principal: Math.round((emi - balance*interestperterm)*100)/100,
+            balance: Math.round((balance - (emi - balance*interestperterm))*100)/100
+          }
+          if (obj.balance<0)
+            obj.balance = 0;
+          $scope.rows.push(obj);
+          balance = balance - obj.principal;
+        }
+      }
+    );
+
   });
