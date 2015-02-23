f8a8ac7b39757216a6ef2b2841ae6123f22af14c
Krishna Srinivas <krishna.srinivas@gmail.com>
2015-02-24 01:56:32 +0530

    add d3 chart to show principal/interest share

--- client/app/calculator-revenue/calculator-revenue.controller.js
+++ client/app/calculator-revenue/calculator-revenue.controller.js
@@ -6,6 +6,7 @@ angular.module('faTestApp')
   		termtype : "1"
   	}
     $scope.rows = [];
+    var dataset = [];
     $scope.revenue = function() {
         return (Math.round($scope.calc.investmentAmount * Math.pow((1 + (($scope.calc.annualInterest*($scope.calc.termtype/12))/100)), $scope.calc.terms) * 100)/100);
     }
@@ -21,6 +22,12 @@ angular.module('faTestApp')
       return Math.round(($scope.emi()*$scope.calc.terms - $scope.calc.investmentAmount)*100)/100;
     }
 
+    var tip = d3.tip()
+      .attr('class', 'd3-tip')
+      .offset([-10, 0])
+      .html(function(d) {
+          return "<span style='color:red'>" + d.type + " " + d.y + "</span>"
+      });
     $scope.$watch($scope.emi,
       function() {
         $scope.rows = [];
@@ -41,7 +48,70 @@ angular.module('faTestApp')
             obj.balance = 0;
           $scope.rows.push(obj);
         }
+
+        var dataset1 = [];
+        var dataset2 = [];
+        for (var i = 0; i < $scope.rows.length; i++) {
+          dataset1.push({
+            x: i,
+            y: $scope.rows[i].principal,
+            type: "principal"
+          })
+          dataset2.push({
+            x: i,
+            y: $scope.rows[i].interest,
+            type: "interest"
+          })
+        }
+        dataset = [dataset2, dataset1];
+        var stack = d3.layout.stack();
+        stack(dataset);
+        var h = 600;
+        var w = 1200;
+        var xScale = d3.scale.ordinal()
+          .domain(d3.range(dataset[0].length))
+          .rangeRoundBands([0, w], 0.05);
+
+        var yScale = d3.scale.linear()
+          .domain([0,       
+            d3.max(dataset, function(d) {
+              return d3.max(d, function(d) {
+                return d.y0 + d.y;
+              });
+            })
+          ])
+          .range([0, h]);
+
+        $('#chartid').html('');
+        var svg = d3.select("#chartid")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h);
+        svg.call(tip);
+        var colors = d3.scale.category10();
+        var groups = svg.selectAll("g")
+          .data(dataset)
+          .enter()
+          .append("g")
+          .style("fill", function(d, i) {
+            return colors(i);
+          });
+        var rects = groups.selectAll("rect")
+          .data(function(d) {return d; })
+          .enter()
+          .append("rect")
+          .attr("x", function(d, i) {
+            return xScale(i);
+          })
+          .attr("y", function(d) {
+            return yScale(d.y0);
+          })
+          .attr("height", function(d) {
+            return yScale(d.y);
+          })
+          .attr("width", xScale.rangeBand())
+          .on("mouseover", tip.show)
+          .on("mouseout", tip.hide)
       }
     );
-
   });
